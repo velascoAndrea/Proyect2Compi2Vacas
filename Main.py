baf3634f4640ffb0cbddc6862b2c6b4626fe96bc
velascoAndrea <velandreas@gmailcom>
2021-12-28 00:15:42 -0600

    cargadeArchivos

--- Main.py
+++ Main.py
@@ -1,6 +1,15 @@
+import os
+from flask import Flask, render_template,request,url_for
+from flask.helpers import flash
+from werkzeug.utils import redirect, secure_filename 
 
-from flask import Flask, render_template
+UPLOAD_FOLDER = './archs/'
+ALLOWED_EXTENSIONS = set(['csv'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 
 @app.route('/')
 def home():
@@ -11,6 +20,15 @@ def home():
 def about():
     return render_template('about.html')
 
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['myfile']
+      filename = secure_filename(f.filename)
+      f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      return redirect(url_for('home'))
+		
 
 if __name__ == '__main__':
     app.run(debug=True)
